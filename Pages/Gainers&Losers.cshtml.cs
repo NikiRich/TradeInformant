674298a0469892549c1536f339971e4484bc8853
NikiRich <bogatyveron3@gmail.com>
2023-10-08 17:34:15 +0300

    v 0.0.1 -Removed some initial files -Added all the pages and links to them in Layout -Added all css files for all pages

---
+++ Pages/Gainers&Losers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TradeInformant.Pages
+{
+    public class Gainers_LosersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/US_Economic_Indicators.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TradeInformant.Pages
+{
+    public class US_Economic_IndicatorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
