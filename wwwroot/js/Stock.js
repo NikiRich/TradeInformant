9f19047df66a56c89fbe166219c185ec35aa162a
NikiRich <bogatyveron3@gmail.com>
2023-10-20 21:59:15 +0300

    v 0.1.1
    -Added initial stock js info

--- wwwroot/js/Stock.js
+++ wwwroot/js/Stock.js
@@ -1,1 +1,12 @@
-﻿
+﻿document.getElementById("StockForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const stockName = document.getElementById("stockName").value;
+    const interval = document.getElementById("interval").value;
+    const periods = parseInt(document.getElementById("periods").value);
+});
+
+if (isNaN(periods) || periods < 1 || periods > 100) {
+    alert("Please enter a valid number of periods.");
+    return;
+}
