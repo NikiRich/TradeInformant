5f8c1c7f05df1cbdbde0c913d1bd28d7dfc09126
NikiRich <bogatyveron3@gmail.com>
2023-10-22 01:05:59 +0300

    v 0.1.5
    -Added the C# Stocks

--- Pages/Stocks.cshtml.cs
+++ Pages/Stocks.cshtml.cs
@@ -1,12 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
 
 namespace TradeInformant.Pages
 {
     public class StocksModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public string stockName { get; set; }
+
+        [BindProperty]
+        public string interval { get; set; }
+
+        [BindProperty]
+        public int periods { get; set; }
+
+        public IActionResult OnPost()
         {
+            const string API_KEY = "9UIS088TUP5KK1QB";
+
+            string function;
+
+            switch(interval)
+            {
+                case "Daily":
+                    function = "TIME_SERIES_DAILY";
+                    break;
+                case "Weekly":
+                    function = "TIME_SERIES_WEEKLY";
+                    break;
+                case "Monthly":
+                    function = "TIME_SERIES_MONTHLY";
+                    break;
+                default:
+                    return new BadRequestObjectResult("Invalid interval");
+            }
+
+            string url = $"https://www.alphavantage.co/query?function={function}&symbol={stockName}&apikey={API_KEY}";
+            Uri uri = new Uri(url);
+
+            Dictionary<string, dynamic> jsonInfo;
+
+            using (WebClient client = new WebClient())
+            {
+                jsonInfo = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(client.DownloadString(uri));
+            }
+
+            if (jsonInfo == null || !jsonInfo.ContainsKey("Meta Data"))
+            {
+                return new BadRequestObjectResult("Error retrieving data for the particular stock or invalid data format");
+            }
+            return new JsonResult(jsonInfo);
         }
+
     }
 }
