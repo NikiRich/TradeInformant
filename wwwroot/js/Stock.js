13072e8e10467fa4a016dcbf76c4a4b8a5b492d4
NikiRich <bogatyveron3@gmail.com>
2023-10-21 15:21:05 +0300

    v. 0.1.4 -Added initial output data to Stock JS without the indicators

--- wwwroot/js/Stock.js
+++ wwwroot/js/Stock.js
@@ -4,34 +4,34 @@
     const stockName = document.getElementById("stockName").value;
     const interval = document.getElementById("interval").value;
     const periods = parseInt(document.getElementById("periods").value);
+
+    if (isNaN(periods) || periods < 1 || periods > 100) {
+        alert("Please enter a valid number of periods.");
+        return;
+    }
+
+    const stockForm = new FormData();
+    stockForm.append("stockName", stockName);
+    stockForm.append("interval", interval);
+    stockForm.append("periods", periods);
+
+    fetch("/Stock", {
+        method: "POST",
+        body: stockForm
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayStock(data, interval);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
 });
-
-if (isNaN(periods) || periods < 1 || periods > 100) {
-    alert("Please enter a valid number of periods.");
-    return;
-}
-
-const stockForm = new FormData();
-stockForm.append("stockName", stockName);
-stockForm.append("interval", interval);
-stockForm.append("periods", periods);
-
-fetch("/Stock", {
-    method: "POST",
-    body: stockForm
-})
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        displayStock(data, interval);
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
 
 function displayStock(data, interval) {
     const output = document.getElementById("StockResult");
@@ -55,4 +55,19 @@ function displayStock(data, interval) {
     }
 
     const timeSeries = data[timeSeriesDate];
+
+    const dates = Object.keys(timeSeries).slice(0, number);
+
+    for (const date of dates) {
+        const stockInfo = timeSeries[date];
+        output.innerHTML += `
+        <div id="StockOutput">
+            <p>Date: ${date}</p>
+            <p>Open: ${stockInfo["1. open"]}</p>
+            <p>High: ${stockInfo["2. high"]}</p>
+            <p>Low: ${stockInfo["3. low"]}</p>
+            <p>Close: ${stockInfo["4. close"]}</p>
+        </div>
+        `
+    }
 }
