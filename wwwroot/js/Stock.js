f05db6a0b4101d60c3854f16ed2e0b73571329f9
NikiRich <bogatyveron3@gmail.com>
2023-10-21 11:14:41 +0300

    v. 0.1.2
    -Added some functionality to the Stock JS

--- wwwroot/js/Stock.js
+++ wwwroot/js/Stock.js
@@ -10,3 +10,34 @@ if (isNaN(periods) || periods < 1 || periods > 100) {
     alert("Please enter a valid number of periods.");
     return;
 }
+
+const stockForm = new FormData();
+stockForm.append("stockName", stockName);
+stockForm.append("interval", interval);
+stockForm.append("periods", periods);
+
+fetch("/Stock", {
+    method: "POST",
+    body: stockForm
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayStock(data);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+
+function displayStock(data) {
+    const output = document.getElementById("StockResult");
+    output.innerHTML = "";
+
+    data.forEach(stock => {
+       
+    });
+}
