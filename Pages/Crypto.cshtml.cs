c65f681bf519f8eff274ac3d490889bb888676dd
NikiRich <bogatyveron3@gmail.com>
2023-10-24 18:37:42 +0300

    v. 2.0.4
    -Added the Crypto backend

--- Pages/Crypto.cshtml.cs
+++ Pages/Crypto.cshtml.cs
@@ -1,12 +1,73 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
 
 namespace TradeInformant.Pages
 {
-    public class CryptoModel : PageModel
+    public class CryptoModel : PageModel // Adjusted the class name for clarity
     {
-        public void OnGet()
+        public string stockName { get; set; }
+        public string interval { get; set; }
+        public string market { get; set; }
+        public int periods { get; set; }
+
+        public IActionResult OnGet(string? stockName, string? interval, string? market, int? periods)
         {
+            if (stockName == null || interval == null || market == null || periods == null)
+            {
+                return Page();
+            }
+
+            // Assign the properties
+            this.stockName = stockName;
+            this.interval = interval;
+            this.market = market;
+            this.periods = (int)periods;
+
+            const string API_KEY = "1F6SLA57L4NZM1DR";
+
+            string function;
+
+            switch (interval)
+            {
+                case "Daily":
+                    function = "DIGITAL_CURRENCY_DAILY";
+                    break;
+                case "Weekly":
+                    function = "DIGITAL_CURRENCY_WEEKLY";
+                    break;
+                case "Monthly":
+                    function = "DIGITAL_CURRENCY_MONTHLY";
+                    break;
+                default:
+                    return new BadRequestObjectResult($"Invalid interval: {interval}");
+            }
+
+            string url = $"https://www.alphavantage.co/query?function={function}&symbol={stockName}&market={market}&apikey={API_KEY}";
+            Uri uri = new Uri(url);
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    var jsonInfo = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(client.DownloadString(uri));
+
+                    if (jsonInfo == null)
+                    {
+                        Console.WriteLine($"Interval: {interval}, CryptoName: {stockName}, Market: {market}");
+                        return new BadRequestObjectResult("Error retrieving data for the particular cryptocurrency or invalid data format");
+                    }
+                    return new JsonResult(jsonInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching cryptocurrency data for {stockName} with interval {interval} and market {market}: {ex.Message}");
+                return new BadRequestObjectResult("Error fetching cryptocurrency data. Please try again later.");
+            }
         }
     }
 }
