1d99538a9aca77c9cc9346f1d25b59b1d4390105
NikiRich <bogatyveron3@gmail.com>
2023-10-26 14:13:57 +0300

    v. 2.0.9
    -Added Cache to the Crypto file

--- Pages/Crypto.cshtml.cs
+++ Pages/Crypto.cshtml.cs
@@ -4,10 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text.Json;
+using static TradeInformant.Pages.StocksModel;
 
 namespace TradeInformant.Pages
 {
-    public class CryptoModel : PageModel 
+    public class CryptoModel : PageModel
     {
         public string stockName { get; set; }
         public string interval { get; set; }
@@ -15,6 +16,42 @@ namespace TradeInformant.Pages
         public int periods { get; set; }
 
         private static readonly string cryptoFile = "cryptoCache.json";
+        private static readonly TimeSpan CacheDuration = TimeSpan.FromHours(72);
+
+        public class CacheEntry
+        {
+            public Dictionary<string, dynamic> Data { get; set; }
+            public DateTime Timestamp { get; set; }
+        }
+
+        public Dictionary<string, dynamic> LoadCacheFromFile(string path)
+        {
+            if (System.IO.File.Exists(cryptoFile))
+            {
+                var jsonString = System.IO.File.ReadAllText(cryptoFile);
+                var cacheEntry = JsonSerializer.Deserialize<CacheEntry>(jsonString);
+
+                if (DateTime.UtcNow - cacheEntry.Timestamp < CacheDuration)
+                {
+                    return cacheEntry.Data;
+                }
+            }
+            return null;
+        }
+
+        public void SaveCacheToFile(Dictionary<string, dynamic> data, string path)
+        {
+            var cacheEntry = new CacheEntry
+            {
+                Data = data,
+                Timestamp = DateTime.UtcNow
+            };
+
+            lock (cryptoFile)
+            {
+                System.IO.File.WriteAllText(cryptoFile, JsonSerializer.Serialize(cacheEntry));
+            }
+        }
 
 
         public IActionResult OnGet(string? stockName, string? interval, string? market, int? periods)
@@ -24,7 +61,6 @@ namespace TradeInformant.Pages
                 return Page();
             }
 
-            // Assign the properties
             this.stockName = stockName;
             this.interval = interval;
             this.market = market;
@@ -52,25 +88,31 @@ namespace TradeInformant.Pages
             string url = $"https://www.alphavantage.co/query?function={function}&symbol={stockName}&market={market}&apikey={API_KEY}";
             Uri uri = new Uri(url);
 
-            try
-            {
-                using (WebClient client = new WebClient())
-                {
-                    var jsonInfo = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(client.DownloadString(uri));
+            Dictionary<string, dynamic> jsonInfo = LoadCacheFromFile(cryptoFile);
 
-                    if (jsonInfo == null)
+            if (jsonInfo == null)
+            {
+                try
+                {
+                    using (WebClient client = new WebClient())
                     {
-                        Console.WriteLine($"Interval: {interval}, CryptoName: {stockName}, Market: {market}");
-                        return new BadRequestObjectResult("Error retrieving data for the particular cryptocurrency or invalid data format");
+                        jsonInfo = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(client.DownloadString(uri));
+
+                        if (jsonInfo == null)
+                        {
+                            Console.WriteLine($"Interval: {interval}, CryptoName: {stockName}, Market: {market}");
+                            return new BadRequestObjectResult("Error retrieving data for the particular cryptocurrency or invalid data format");
+                        }
+                        return new JsonResult(jsonInfo);
                     }
-                    return new JsonResult(jsonInfo);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error fetching cryptocurrency data for {stockName} with interval {interval} and market {market}: {ex.Message}");
+                    return new BadRequestObjectResult("Error fetching cryptocurrency data. Please try again later.");
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error fetching cryptocurrency data for {stockName} with interval {interval} and market {market}: {ex.Message}");
-                return new BadRequestObjectResult("Error fetching cryptocurrency data. Please try again later.");
-            }
+            return new JsonResult(jsonInfo);
         }
     }
 }
