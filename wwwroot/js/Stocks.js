86d3efb23f580163d6ad447ecf49e8b817bb3d93
NikiRich <bogatyveron3@gmail.com>
2023-11-01 19:01:43 +0200

    v. 4.3
    -Added MACD, Signal Line and RSI

--- wwwroot/js/Stocks.js
+++ wwwroot/js/Stocks.js
@@ -36,12 +36,14 @@ document.getElementById("StockForm").addEventListener("submit", function (event)
         .then(data => {
             // Extract the time series data based on the selected interval.
             const timeSeries = getTimeSeries(data, interval);
+
             // Compute SMA and EMA values.
             const SMA = computeSMA(timeSeries, periods);
             const EMA = computeEMA(timeSeries, periods);
             const RSI = computeRSI(timeSeries, periods);
+            const MACD = computeMACD(timeSeries);
             // Display the stock information.
-            displayStock(data, interval, periods, SMA, EMA, RSI);
+            displayStock(data, interval, periods, SMA, EMA, RSI, MACD);
         })
         .catch(error => {
             // Handle any errors that occurred during the fetch.
@@ -78,34 +80,39 @@ function computeSMA(timeSeries, periods) {
 
 
 // Compute the Exponential Moving Average (EMA) for the given period.
-function computeEMA(timeSeries, periods) {
-    // If the timeSeries data is not available, return null.
-    if (!timeSeries) return null;
+function computeEMA(input, periods) {
+    let closingPrices;
 
-    // Extract and map the closing prices from the time series data.
-    const closingPrices = Object.values(timeSeries).map(day => parseFloat(day["4. close"]));
+    // Check if input is an array, then use it directly as the closing prices.
+    if (Array.isArray(input)) {
+        closingPrices = input;
+    } else if (input && typeof input === 'object') {
+        // If it's an object, extract the closing prices using the '4. close' key.
+        closingPrices = Object.values(input).map(day => parseFloat(day["4. close"]));
+    } else {
+        // If the input is neither an array nor a valid object, return null.
+        return null;
+    }
 
     // Calculate the initial Simple Moving Average (SMA) for the given periods.
-    // This will serve as our starting point for the EMA calculation.
     const initialSMA = closingPrices.slice(0, periods).reduce((acc, price) => acc + price, 0) / periods;
 
     // Initialize the EMA array with the initial SMA value.
     let EMA = [initialSMA];
 
-    // Calculate the multiplier using the formula: 2 / (periods + 1).
-    // This factor decides the weightage of recent prices vs previous EMA in the EMA formula.
+    // Calculate the multiplier for the EMA calculation.
     const multiplier = 2 / (periods + 1);
 
-    // Starting from the next day after initial SMA, calculate the EMA.
-    // EMA formula: (Close - Previous EMA) * multiplier + Previous EMA.
+    // Calculate the EMA for each subsequent closing price.
     for (let i = periods; i < closingPrices.length; i++) {
-        const newEMA = (closingPrices[i] * multiplier) + (EMA[EMA.length - 1] * (1 - multiplier));
+        const newEMA = (closingPrices[i] - EMA[EMA.length - 1]) * multiplier + EMA[EMA.length - 1];
         EMA.push(newEMA);
     }
 
-    // Return the calculated EMA values.
+    // Return the EMA array.
     return EMA;
 }
+
 
 
 function computeRSI(timeSeries, periods) {
@@ -114,20 +121,25 @@ function computeRSI(timeSeries, periods) {
 
     const closingPrices = Object.values(timeSeries).map(day => parseFloat(day["4. close"]));
 
+    // Calculate the gains and losses for each day.
     for (let i = 1; i < closingPrices.length; i++) {
         let change = closingPrices[i] - closingPrices[i - 1];
         gains.push(Math.max(change, 0)); // If change is negative, push 0
         losses.push(Math.max(-change, 0)); // If change is positive or 0, push 0
     }
 
+    // Calculate the average gain and average loss for the first N days.
     let avgGain = gains.slice(0, periods).reduce((acc, gain) => acc + gain, 0) / periods;
     let avgLoss = losses.slice(0, periods).reduce((acc, loss) => acc + loss, 0) / periods;
 
+    // Calculate the initial RSI value.
     let RS = avgGain / avgLoss;
     let initialRSI = 100 - (100 / (1 + RS));
 
-    let RSI = [initialRSI];  // Initialize RSI as an array with the initial RSI value.
+    // Initialize RSI as an array with the initial RSI value.
+    let RSI = [initialRSI];
 
+    // Calculate the RSI for the remaining days.
     for (let i = periods; i < gains.length; i++) {
         avgGain = ((avgGain * (periods - 1)) + gains[i]) / periods;
         avgLoss = ((avgLoss * (periods - 1)) + losses[i]) / periods;
@@ -140,10 +152,43 @@ function computeRSI(timeSeries, periods) {
     return RSI;
 }
 
+// Compute the Moving Average Convergence Divergence (MACD) for the given period.
+function computeMACD(timeSeries) {
+    const closingPrices = Object.values(timeSeries).map(day => parseFloat(day["4. close"]));
 
+    //Compute the 12-period EMA and 26-period EMA
+    let shortEMA = computeEMA(timeSeries, 12);
+    let longEMA = computeEMA(timeSeries, 26);
+
+    //Compute MACD Line
+    let MACD = [];
+    for (let i = 0; i < closingPrices.length; i++) {
+        // Ensure that we have values for both EMA12 and EMA26 before subtracting.
+        if (shortEMA[i] !== undefined && longEMA[i] !== undefined) {
+            MACD[i] = shortEMA[i] - longEMA[i];
+        }
+    }
+
+    //Compute Signal Line
+    let signalLine = computeEMA(MACD, 9);
+
+    //Compute MACD Histogram
+    let histogram = [];
+    for (let i = 0; i < MACD.length; i++) {
+        if (MACD[i] !== undefined && signalLine[i] !== undefined) {
+            histogram[i] = MACD[i] - signalLine[i];
+        }
+    }
+
+    return {
+        MACD: MACD,
+        signalLine: signalLine,
+        histogram: histogram
+    };
+}
 
 // Display stock data, SMA, and EMA on the web page.
-function displayStock(data, interval, periods, SMA, EMA, RSI) {
+function displayStock(data, interval, periods, SMA, EMA, RSI, MACD) {
     const output = document.getElementById("StockResult");
 
     let timeSeriesDate;
@@ -175,12 +220,19 @@ function displayStock(data, interval, periods, SMA, EMA, RSI) {
     // Extract the first N dates from the time series data.
     const dates = Object.keys(timeSeries).slice(0, periods);
 
+    let lastMACDValue = (MACD.MACD && MACD.MACD.length > 0) ? MACD.MACD[MACD.MACD.length - 1].toFixed(2) : "N/A";
+    let signalLineValue = (MACD.signalLine && MACD.signalLine.length > 0) ? MACD.signalLine[MACD.signalLine.length - 1].toFixed(2) : "N/A";
+    let histogramValue = (MACD.histogram && MACD.histogram.length > 0) ? MACD.histogram[MACD.histogram.length - 1].toFixed(2) : "N/A";
+
     // Update the output with the SMA and EMA values.
     output.innerHTML = ` 
     <div>
         <p><strong>SMA:</strong> ${SMA.toFixed(2)}</p>
         <p><strong>EMA:</strong> ${EMA[EMA.length - 1].toFixed(2)}</p>
         <p><strong>RSI:</strong> ${RSI[RSI.length - 1].toFixed(2)}</p>
+        <p><strong>MACD:</strong> ${lastMACDValue}</p>
+        <p><strong>Signal Line:</strong> ${signalLineValue}</p>
+        <p><strong>Histogram:</strong> ${histogramValue}</p>
     </div>
     `;
 
