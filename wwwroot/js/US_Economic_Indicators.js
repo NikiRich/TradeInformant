c76e8da634e4348159223c3809984940cb16addb
NikiRich <bogatyveron3@gmail.com>
2023-11-25 15:53:16 +0200

    v. 7.5
    -Added all front end for US_Economic_Indicators

--- wwwroot/js/US_Economic_Indicators.js
+++ wwwroot/js/US_Economic_Indicators.js
@@ -34,8 +34,8 @@ document.getElementById("RealGDPForm").addEventListener("submit", function (even
 
 function DisplayQuery1(result, RealGDPperiod) {
     const RealGDPOutput = document.getElementById("RealGDPOutput");
-    RealGDPOutput.innerHTML = '';  
-    const timeSeries = result["data"].slice(0, RealGDPperiod);  
+    RealGDPOutput.innerHTML = '';
+    const timeSeries = result["data"].slice(0, RealGDPperiod);
 
     for (const entry of timeSeries) {
         RealGDPOutput.innerHTML += `
@@ -53,23 +53,223 @@ function DisplayQuery1(result, RealGDPperiod) {
     }
 }
 
+document.getElementById("RealGDPperCapitaForm").addEventListener("submit", function (event) {
+    // Preventing the form from being submitted
+    event.preventDefault();
+    // Getting the value from the form
+    const RealGDPperCapitaPeriod = document.getElementById("RealGDPperCapitaPeriod").value;
+    // Validating the input
+    if (RealGDPperCapitaPeriod < 1 || RealGDPperCapitaPeriod > 100) {
+        alert("Enter a valid number")
+        return;
+    }
+    // Prepaing the query string for the API
+    const query2 = new URLSearchParams({
+        RealGDPperCapitaPeriod: RealGDPperCapitaPeriod,
+    });
 
+    fetch(`/US_Economic_Indicators?handler=RGDPpCP&${query2.toString()}`, {
+        method: "GET"
+    })
 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            };
+            return response.json();
+        })
+        .then(result => {
+            DisplayQuery2(result, RealGDPperCapitaPeriod);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        })
+})
 
+function DisplayQuery2(result, RealGDPperCapitaPeriod) {
+    const RealGDPperCapitaOutput = document.getElementById("RealGDPperCapitaOutput");
+    RealGDPperCapitaOutput.innerHTML = '';
+    const timeSeries = result["data"].slice(0, RealGDPperCapitaPeriod);
 
-
-
-
-
-
-/*
-if ((RealGDPperiod < 1 || RealGDPperiod > 100) || (RealGDPperCapitaPeriod < 1 || RealGDPperCapitaPeriod > 100) ||
-    (CPIperiod < 1 || CPIperiod > 100) || (InflationPeriod < 1 || InflationPeriod > 100) || (UnemploymentRatePeriod < 1 || UnemploymentRatePeriod > 100)) {
-    alert("Enter a valid number")
-    return;
+    for (const entry of timeSeries) {
+        RealGDPperCapitaOutput.innerHTML += `
+        <div class="col-md-2">
+            <div class="card md-2">
+                <div class="card-header">
+                    Date:${entry.date}
+                </div>
+                <div class="card-body">
+                    <p><strong>Real GDP Per Capita:</strong> ${entry.value}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
 }
-const CPI = document.getElementById("CPI").value;
-const CPIperiod = document.getElementById("CPIperiod").value;
-const InflationPeriod = document.getElementById("InflationPeriod").value;
-const UnemploymentRatePeriod = document.getElementById("UnemploymentRatePeriod").value;
-*/
+
+
+document.getElementById("CPIForm").addEventListener("submit", function (event) {
+    // Preventing the form from being submitted
+    event.preventDefault();
+    // Getting the value from the form
+    const CPIperiod = document.getElementById("CPIperiod").value;
+    const CPI = document.getElementById("CPI").value;
+    // Validating the input
+    if (CPIperiod < 1 || CPIperiod > 100) {
+        alert("Enter a valid number")
+        return;
+    }
+    // Prepaing the query string for the API
+    const query3 = new URLSearchParams({
+        CPIperiod: CPIperiod,
+        CPI: CPI
+    });
+
+    fetch(`/US_Economic_Indicators?handler=CPI&${query3.toString()}`, {
+        method: "GET"
+    })
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            };
+            return response.json();
+        })
+        .then(result => {
+            DisplayQuery3(result, CPIperiod);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        })
+})
+
+
+function DisplayQuery3(result, CPIperiod) {
+    const CPIOutput = document.getElementById("CPIOutput");
+    CPIOutput.innerHTML = '';
+    const timeSeries = result["data"].slice(0, CPIperiod);
+
+    for (const entry of timeSeries) {
+        RealGDPperCapitaOutput.innerHTML += `
+        <div class="col-md-2">
+            <div class="card md-2">
+                <div class="card-header">
+                    Date:${entry.date}
+                </div>
+                <div class="card-body">
+                    <p><strong>CPI:</strong> ${entry.value}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+}
+
+
+document.getElementById("InflationForm").addEventListener("submit", function (event) {
+    // Preventing the form from being submitted
+    event.preventDefault();
+    // Getting the value from the form
+    const InflationPeriod = document.getElementById("InflationPeriod").value;
+    // Validating the input
+    if (InflationPeriod < 1 || InflationPeriod > 100) {
+        alert("Enter a valid number")
+        return;
+    }
+    // Prepaing the query string for the API
+    const query4 = new URLSearchParams({
+        InflationPeriod: InflationPeriod,
+    });
+
+    fetch(`/US_Economic_Indicators?handler=Inflation&${query4.toString()}`, {
+        method: "GET"
+    })
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            };
+            return response.json();
+        })
+        .then(result => {
+            DisplayQuery4(result, InflationPeriod);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        })
+})
+
+function DisplayQuery4(result, InflationPeriod) {
+    const InflationOutput = document.getElementById("InflationOutput");
+    InflationOutput.innerHTML = '';
+    const timeSeries = result["data"].slice(0, InflationPeriod);
+
+    for (const entry of timeSeries) {
+        InflationOutput.innerHTML += `
+        <div class="col-md-2">
+            <div class="card md-2">
+                <div class="card-header">
+                    Date:${entry.date}
+                </div>
+                <div class="card-body">
+                    <p><strong>Inflation:</strong> ${entry.value}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+}
+
+document.getElementById("InflationForm").addEventListener("submit", function (event) {
+    // Preventing the form from being submitted
+    event.preventDefault();
+    // Getting the value from the form
+    const UnemploymentRatePeriod = document.getElementById("UnemploymentRatePeriod").value;
+    // Validating the input
+    if (UnemploymentRatePeriod < 1 || UnemploymentRatePeriod > 100) {
+        alert("Enter a valid number")
+        return;
+    }
+    // Prepaing the query string for the API
+    const query5 = new URLSearchParams({
+        UnemploymentRatePeriod: UnemploymentRatePeriod,
+    });
+
+    fetch(`/US_Economic_Indicators?handler=Unemployment&${query5.toString()}`, {
+        method: "GET"
+    })
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            };
+            return response.json();
+        })
+        .then(result => {
+            DisplayQuery5(result, UnemploymentRatePeriod);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        })
+})
+
+function DisplayQuery5(result, UnemploymentRatePeriod) {
+    const UnemploymentRateOutput = document.getElementById("UnemploymentRateOutput");
+    UnemploymentRateOutput.innerHTML = '';
+    const timeSeries = result["data"].slice(0, UnemploymentRatePeriod);
+
+    for (const entry of timeSeries) {
+        UnemploymentRateOutput.innerHTML += `
+        <div class="col-md-2">
+            <div class="card md-2">
+                <div class="card-header">
+                    Date:${entry.date}
+                </div>
+                <div class="card-body">
+                    <p><strong>Inflation:</strong> ${entry.value}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+}
